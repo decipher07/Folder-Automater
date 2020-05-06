4024632e48773b64c9ee6864e8c2844d3924f694
decipher07 <social.deej@gmail.com>
2020-05-06 17:39:05 +0530

    Working Perfectly

--- automatePDF.py
+++ automatePDF.py
@@ -2,4 +2,10 @@ import os
 import shutil 
 
 os.chdir('/home/nopc/Downloads/SCRAP DOWNLOAD')
-print()
+
+path_to_new_folder = os.getcwd() + '/PDFINHERE'
+for f in os.listdir():
+    if f.endswith('.pdf'):
+        current_file = os.getcwd() + '/'+ f
+        shutil.move(current_file, path_to_new_folder)
+        print("Success")
