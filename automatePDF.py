8ecb1c474985d26feabe8196d53f2ff30c0c07ce
decipher07 <social.deej@gmail.com>
2020-05-06 18:28:12 +0530

    Changing It to Pile All Files

--- automatePDF.py
+++ automatePDF.py
@@ -3,9 +3,26 @@ import shutil
 
 os.chdir('/home/nopc/Downloads/SCRAP DOWNLOAD')
 
-path_to_new_folder = os.getcwd() + '/PDFINHERE'
-for f in os.listdir():
-    if f.endswith('.pdf'):
-        current_file = os.getcwd() + '/'+ f
-        shutil.move(current_file, path_to_new_folder)
-        print("Success")
+def move_pdf ():
+    path_to_new_folder = os.getcwd() + '/PDFINHERE'
+    for f in os.listdir():
+        if f.endswith('.pdf'):
+            current_file = os.getcwd() + '/'+ f
+            shutil.move(current_file, path_to_new_folder)
+            print("Success")
+
+list_having_all_extensions = [] 
+
+def findall () :
+    for f in os.listdir():
+        ch = f.split('.')
+        if len(ch) == 2:
+            list_having_all_extensions.append(ch[1])
+    removing_duplicates()    
+    
+def removing_duplicates ():
+    unique_extensions = list(dict.fromkeys(list_having_all_extensions))
+    print(unique_extensions)
+
+
+findall()
