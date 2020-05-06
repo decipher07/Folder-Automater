80ad1c5848d22dc150dac36f41f88f4c7c7ad03e
decipher07 <social.deej@gmail.com>
2020-05-06 18:45:54 +0530

    Collected All Photos

--- automatePDF.py
+++ automatePDF.py
@@ -12,6 +12,7 @@ def move_pdf ():
             print("Success")
 
 list_having_all_extensions = [] 
+unique_extensions = []
 
 def findall () :
     for f in os.listdir():
@@ -19,10 +20,21 @@ def findall () :
         if len(ch) == 2:
             list_having_all_extensions.append(ch[1])
     removing_duplicates()    
-    
+
 def removing_duplicates ():
     unique_extensions = list(dict.fromkeys(list_having_all_extensions))
     print(unique_extensions)
 
+def combining_all_pictures () :
+    new_folder_path = os.getcwd() + '/Photos'
+    #os.mkdir(new_folder_path)
+    for f in os.listdir():
+        ch = f.split('.')
+        if len(ch) == 2:
+            if ch[1] == 'jpeg' or ch[1] == 'png' or ch[1] == 'jpg' or ch[1] == 'obj':
+                current_file = os.getcwd() + '/'+ f
+                shutil.move(current_file, new_folder_path)
+                print("Success")
 
 findall()
+combining_all_pictures()
